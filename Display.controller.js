c8812fb29ff99afcd387112ceb6e9589abca96cf
canercosar <96355045+canercosar@users.noreply.github.com>
2022-02-07 01:10:57 +0300

    Update Display.controller.js

--- Display.controller.js
+++ Display.controller.js
@@ -3,7 +3,7 @@ sap.ui.controller("zzsimpleform2.Display", {
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf zzorderform2.Display
+* @memberOf zzsimpleform2.Display
 */
 onInit: function() {
 
@@ -21,7 +21,7 @@ onInit: function() {
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf zzorderform2.Display
+* @memberOf zzsimpleform2.Display
 */
 onBeforeRendering: function() {
 
@@ -33,7 +33,7 @@ onBeforeRendering: function() {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf zzorderform2.Display
+* @memberOf zzsimpleform2.Display
 */
 //	onAfterRendering: function() {
 //
@@ -41,7 +41,7 @@ onBeforeRendering: function() {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf zzorderform2.Display
+* @memberOf zzsimpleform2.Display
 */
 //	onExit: function() {
 //
